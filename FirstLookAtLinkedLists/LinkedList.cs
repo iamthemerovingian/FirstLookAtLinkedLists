ed65008a68f9dd95267a1f73ed7d6fa7cbdd4c0b
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-01-31 15:24:10 +0700

    Add to front and Add to end are both workiing. but its not the best solution.

---
+++ FirstLookAtLinkedLists/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿
+namespace FirstLookAtLinkedLists
+{
+    class LinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+    }
+
+    class LinkedList<T>
+    {
+        public LinkedListNode<T> Head { get; private set; }
+
+        public LinkedListNode<T> Tail { get; private set; }
+        public int Count { get; private set; }
+
+        public void AddToFront(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = Head;
+            Head = node;
+            node.Next = temp;
+            Count++;
+
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+        }
+
+        public void AddToEnd(LinkedListNode<T> node)
+        {
+            if (Count == 0)
+            {
+                AddToFront(node);
+            }
+            else
+            {
+                Count++;
+                Tail.Next = node;
+                Tail = node;
+            }
+        }
+    }
+}
--- FirstLookAtLinkedLists/Program.cs
+++ FirstLookAtLinkedLists/Program.cs
@@ -10,6 +10,24 @@ namespace FirstLookAtLinkedLists
     {
         static void Main(string[] args)
         {
+            LinkedList<int> myFirstLinkedList = new LinkedList<int>();
+            LinkedList<int> mySecondLinkedList = new LinkedList<int>();
+
+            LinkedListNode<int> nodeOne = new LinkedListNode<int>() { Value = 1 };
+            LinkedListNode<int> nodeTwo = new LinkedListNode<int>() { Value = 2 };
+            LinkedListNode<int> nodeThree = new LinkedListNode<int>() { Value = 3 };
+
+            myFirstLinkedList.AddToFront(nodeOne);
+            myFirstLinkedList.AddToFront(nodeTwo);
+            myFirstLinkedList.AddToFront(nodeThree);
+
+            Console.WriteLine("Count is: " + myFirstLinkedList.Count);
+
+            mySecondLinkedList.AddToEnd(nodeOne);
+            mySecondLinkedList.AddToEnd(nodeTwo);
+            mySecondLinkedList.AddToEnd(nodeThree);
+
+            Console.WriteLine("Count is: " + mySecondLinkedList.Count);
         }
     }
 }
