502b570e36d6ef35f8e8ebf37ab38b8aba46347b
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-01-31 14:29:56 +0700

    Add project files.

---
+++ FirstLookAtLinkedLists/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLookAtLinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
