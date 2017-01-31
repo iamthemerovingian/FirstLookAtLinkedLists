ba2431153c22dea65521f899cb8c90eb2e34a96e
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-01-31 17:12:51 +0700

    Made the LinkedList implement the ICollections Interface. Added a method for contains.

--- FirstLookAtLinkedLists/LinkedList.cs
+++ FirstLookAtLinkedLists/LinkedList.cs
@@ -16,12 +16,25 @@ namespace FirstLookAtLinkedLists
         public LinkedListNode<T> Next { get; set; }
     }
 
-    class LinkedList<T> : System.Collections.Generic.IEnumerable<T>
+    class LinkedList<T> : ICollection<T>
     {
         public LinkedListNode<T> Head { get; private set; }
 
         public LinkedListNode<T> Tail { get; private set; }
         public int Count { get; private set; }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void AddFirst(T value)
+        {
+            AddToFront(new LinkedListNode<T>(value));
+        }
 
         public void AddToFront(LinkedListNode<T> node)
         {
@@ -34,6 +47,11 @@ namespace FirstLookAtLinkedLists
             {
                 Tail = Head;
             }
+        }
+
+        public void AddLast(T value)
+        {
+            AddToEnd(new LinkedListNode<T>(value));
         }
 
         public void AddToEnd(LinkedListNode<T> node)
@@ -112,6 +130,42 @@ namespace FirstLookAtLinkedLists
                 yield return currentNode.Value;
                 currentNode = currentNode.Next;
             }
+        }
+
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+        public void Clear()
+        {
+            RemoveFirst();
+        }
+
+        public bool Contains(T item)
+        {
+            LinkedListNode<T> currentNode = Head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Value.Equals(item))
+                {
+                    return true;
+                }
+                currentNode = currentNode.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
         }
     }
 }
